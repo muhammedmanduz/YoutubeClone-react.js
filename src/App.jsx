c141c94cc4251609b95e992339ef1daf0c097675
muhammedmanduz <muhammed.manduz@gmail.com>
2025-01-08 16:28:32 +0300

    Arama Sonucları yapıldı

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Feed from "./pages/Feed";
 import Header from "./components/Header";
 import { VideoProvider } from "./context/videoContext.jsx";
 import Detail from "./pages/Detail";
+import SearchResults from "./pages/SearchResults.jsx";
 
 const App = () => {
   return (
@@ -20,7 +21,8 @@ const App = () => {
             </VideoProvider>
           }
         />
-        <Route path="/watch" element={<Detail />} />"
+        <Route path="/watch" element={<Detail />} />
+        <Route path="/results" element={<SearchResults />} />
       </Routes>
     </BrowserRouter>
   );
